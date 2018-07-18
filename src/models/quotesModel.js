a56e88046549ba1320efeccf4acea0059174d399
daisyndungu <daisywndungu@gmail.com>
2018-07-18 19:37:41 +0300

    refactor code

--- src/models/quotesModel.js
+++ src/models/quotesModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const Quote = new Schema(
-    {author, type: String, require: true},
-    {year, type:Date, require: true},
-    {quote, type:String, require: true}
-);
+const Quote = new Schema({
+    author: {type: String, require: true},
+    year: {type:Date, require: true},
+    quote: {type:String, require: true}
+    });
 
-module.exports(Quote);
+module.exports = mongoose.model('quote', Quote);
